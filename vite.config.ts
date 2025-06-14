09f0284771bedc972fd0ba34f2b0f4ef52d528e0
kanand003 <anand_kumar1999@hotmail.com>
2025-06-14 21:11:40 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/falling-rain/",
+  base: "/falling-rain-assignment/",
   server: {
     host: "::",
     port: 8080,
